e421581fdbb81216712a44722084237c22acb05a
Marten Laane <113144588+plosee@users.noreply.github.com>
2022-11-30 09:47:12 +0200

    Add files via upload

---
+++ ps5.ps1
@@ -0,0 +1,13 @@
+﻿${nimi} = "marten laane"
+${email} = ",marten.laane@gmail.com"
+$date = "suva", "sona", (get-date)
+#$date, $email, $nimi
+	
+$emailid = Get-Content -path C:\users\it22\desktop\emailid.txt
+$massiiv = $emailid + $email
+$split = $massiiv.split(",")
+$split
+
+write-host "emaile on" $split.count
+
+write-host $split[0, 30]
