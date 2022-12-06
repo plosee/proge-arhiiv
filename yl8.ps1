82d10e172e3a449f3596cbf37391ad0f91f88396
Marten Laane <113144588+plosee@users.noreply.github.com>
2022-12-06 10:25:11 +0200

    Add files via upload

---
+++ yl8.ps1
@@ -0,0 +1,36 @@
+﻿
+function mata{
+     <#
+    .SYNOPSIS
+        arvutab pindala ringil
+    .DESCRIPTION
+        arvutab pindala ringil
+    .EXAMPLE
+        arvutab pindala ringil
+    .EXAMPLE
+        arvutab pindala ringil
+    #>
+        param(
+        
+            # Esimese parameetri kirjeldus
+            $r
+         )
+
+
+
+    $s = [Math]::PI*[Math]::Pow($r, 2)
+    $s
+    
+
+    }
+
+
+function matat2($nimi){
+    $nimi.tolower()
+    $nimi.replace("ö", "o").replace("ä", "a").replace("õ", "o").replace("ü", "u")
+    (Get-Culture).TextInfo.ToTitleCase($nimi.tolower())
+    
+    }
+    
+    
+matat2("õüöä")
