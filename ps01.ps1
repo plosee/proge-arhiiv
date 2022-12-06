b7d9eab0a4014e3967f0d8cb272403d2c154fc96
Marten Laane <113144588+plosee@users.noreply.github.com>
2022-12-06 11:11:43 +0200

    Add files via upload

---
+++ ps01.ps1
@@ -0,0 +1,14 @@
+﻿#sama kataloog, mis skriptil
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+ 
+$xml = [xml](Get-Content $dir\customers.xml)
+
+$rows = $xml.customers.customer
+foreach ($row in $rows)
+{if ($row.country -eq "Poland"){
+  $row.full_name
+    }
+
+}
+    
