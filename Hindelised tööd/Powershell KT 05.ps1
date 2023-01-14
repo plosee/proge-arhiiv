1393c0f0859ff5b297881f123008a3a0830bb026
Marten Laane <113144588+plosee@users.noreply.github.com>
2023-01-14 15:40:11 +0200

    Add files via upload

---
+++ Hindelised tööd/Powershell KT 05.ps1
@@ -0,0 +1,16 @@
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+
+$domains = get-content $dir/domains.csv
+new-item $dir/tootavdom.txt
+new-item $dir/ntootavdom.txt
+
+foreach ($domain in $domains) {
+    
+    $error.clear()
+    try { wget $domain -UseBasicParsing }
+    catch { "Error occured" }
+    if (!$error) { $domain >> $dir/tootavdom.txt }
+    else { $domain >> $dir/ntootavdom.txt }
+    
+}    
