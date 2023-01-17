74928061bb7efe3f90dada1368932f975ec8545b
Marten Laane <113144588+plosee@users.noreply.github.com>
2023-01-17 23:35:38 +0200

    Update TKinter Python Ylesanne KT 4.4.py

--- Hindelised tööd/TKinter Python Ylesanne KT 4.4.py
+++ Hindelised tööd/TKinter Python Ylesanne KT 4.4.py
@@ -1,24 +1,38 @@
 from tkinter import *
 
-root = Tk()
-root.title('munandi vaega kontrolltoo')
-root.geometry('300x200')
+##### hahaha naljakas akna seadmed #####
+##### aaa ja marten laane ylesanne #####
 
+root = Tk()                            
+root.title('munandi vaega kontrolltoo')
+root.geometry('300x200')               
+root.resizable(0, 0)                                                         
+
+######################################## 
+
+# siia tuleb jutt mis on 'terv2' funktsioonis.    
 veelykslabel= Label(root, text = 'siia tuleb jutt' )
 veelykslabel.grid(row=3, column=1)
 
+# funktsioon, mis votab entryst/sisestusest kasutaja inputi, teeb selle integeriks
+# siis kasutab seda integeri et muuta L muutujat mis naitab mitu korda tervitust 
+# nii mitu korda kui kasutaja tahtis ja siis muudab 'veelykslabeli' configi
+# mis viskab teksti programmi sisse
+# 
+# mul on pisike onnetus et see tegelikult ei hoia seda malus
+# niiet naeb ainult seda viimast korda kui see vahetub
+# ideeks voib olla teha nii mitu muutujat mis sisestuses ytleb aga see on liiga palju tood
 def terv2():
     arv = int(sisestus.get())
     for i in range(arv):
         L = f"Võõrustaja: 'Tere!' \n Täna {i+1}. kord tervitada, mõtiskleb võõrustaja \n  Külaline: 'Tere, suur tänu kutse eest!'"
-    veelykslabel.config(text = L)
+        veelykslabel.config(text = L)
 
+# siin siis kysib mitu kylalist labeliga ja viskab prorammi entry, mille sisse saab kasutaja panna numbri
 sisestuslabel = Label(root,text='Mitu kylalist tuleb sinna varki?')
 sisestuslabel.grid(row=0,column=1)
 sisestus = Entry(root)
 sisestus.grid(row=1,column=1,columnspan=4)
 
-enter = Button(root, text='enter', command= terv2)
-enter.grid(row=2,column=0)
-
+# ja siis nagu alati on loop lopus et programm tootaks nii kaua kui kinni pannakse
 root.mainloop()
