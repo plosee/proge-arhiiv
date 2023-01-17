13a3163cd736940e1d5d810e2238b3772b222486
Marten Laane <113144588+plosee@users.noreply.github.com>
2023-01-18 00:29:07 +0200

    Update Python Kasutajad KT.py

--- Hindelised tööd/Python Kasutajad KT.py
+++ Hindelised tööd/Python Kasutajad KT.py
@@ -1,33 +1,17 @@
 import random
 
-it21 = open("it21.txt", encoding = "UTF-8")
-failisisu = it21.readlines()
+it22 = open("it21.txt", encoding = "UTF-8")
+failisisu = it22.readlines()
 
-def replaceid(l):
-    for i in l:
+def pullikaka(p):
+    for i in p:
         i = i.lower()
         i = i.replace("ö", "")
         i = i.replace("ä", "")
         i = i.replace("õ", "")
         i = i.replace("ü", "")
-        i= i.replace ("\n", "")
-        em = i.split(" ")
-    
-def emailid(l):
-    replaceid()
-    for i in l:
-        print(f"{i[0]}{em[1]}@gmail.com")
-emailid(failisisu)
-
-def emailid2(l):
-    replaceid()
-    for i in l:
-        print(f"{em[0]}.{em[1]}@gmail.com")
-emailid2(failisisu)
-
-def paroolmunad(l):      
-    for i in l:
-        i = i.lower()
         i = i.replace ("\n", "")
-        print(f"{i[0]}{random.randint(10,999)}")
-paroolmunad(failisisu)
+        em = i.split(" ")
+        print(f"{i[0]}{em[1]}@gmail.com | {em[0]}.{em[1]}@gmail.com | {i[0]}{random.randint(10:)}")
+
+pullikaka(failisisu)      
