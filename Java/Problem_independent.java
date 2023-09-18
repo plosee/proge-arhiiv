02750e0ccba728002339f329f8c5f7c4261c4184
Marten Laane <marten.laane@hkhk.edu.ee>
2023-09-18 20:33:13 +0300

    Update Problem_independent.java

--- Java/Problem_independent.java
+++ Java/Problem_independent.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 
 public class Problem_independent {
     public static void main(String[] args){
- 
+        // kysime kasutaja valikut
         Scanner scan = new Scanner(System.in);
-        System.out.println("Millist programmi soovid? (1-3)");
+        System.out.println("Millist programmi soovid? (1-5)");
         System.out.println("1. Random int");
         System.out.println("2. auto litsents");
         System.out.println("3. keskmine ja summa");
@@ -17,22 +17,22 @@ public class Problem_independent {
         System.out.println("5. minutid tundideks");
         int valik = scan.nextInt();
         scan.close();
-
+        // switch case et naha mis valik tehti
         switch(valik){
             case 1:
-                uno();
+                suvalisednumbrid();
                 break;
             case 2:
-                dos();
+                autolitsentsgenereerija();
                 break;
             case 3:
-                tres();
+                summajakeskmine();
                 break;
             case 4:
-                quatro();
+                binaarjatetsimaal();;
                 break;
             case 5:
-                cinco();
+                tunnidjaminutid();
                 break;
             default:
                 System.out.println("Vale valik");
@@ -41,29 +41,29 @@ public class Problem_independent {
     }
 
     // 10
-    public static void uno(){
+    public static void suvalisednumbrid(){
         // varid asjade jaoks
         Random ran = new Random();
-        String s = "";
+        String stringid = "";
 
         // for loop 5 korda ning lisa stringi numbrid
         for (int i = 0; i < 5; i++){
-            int a = ran.nextInt(66);
-            s += a;
+            int randomint = ran.nextInt(66);
+            stringid += randomint;
         }
 
-        System.out.println(s);
+        System.out.println(stringid);
     }
 
     // 14
-    public static void dos(){
+    public static void autolitsentsgenereerija(){
         // VARID
         Random ran = new Random();
         int min = 99;
         int max = 1000;
-        // randrange calculation
-        int randomnumber = ran.nextInt(max-min) + min;
-        // random 3 tahte genereerimine
+        // randrange kalkulatsioon
+        int randomnumber = ran.nextInt(max-min+1) + min;
+        // suvalise 3 tahte genereerimine
         String tahte = "";
         for (int i = 0; i < 3; i++){
             char c = (char)(ran.nextInt(26) + 'a');
@@ -75,48 +75,42 @@ public class Problem_independent {
     }
 
     // 16
-    public static void tres(){
+    public static void summajakeskmine(){
         // varid
         ArrayList<Integer> list = new ArrayList<Integer>();
         double summa = 0;
         double keskmine = 0;
         // paneme listi numbreid
-        list.add(3);
-        list.add(5);
-        list.add(3);
-        list.add(12);
-        list.add(5);
+        list.add(3); list.add(5); list.add(3); list.add(12); list.add(5);
         // summa arvutus
         for (int i = 0; i < list.size(); i++){
             summa += list.get(i);
         }
         // keskmise arvutus
         keskmine = summa / list.size();
-        // vastuste rassistlik valjastamine
+        // vastuste valjasstamine
         System.out.println("Summa: " + summa);
         System.out.println("Keskmine: " + keskmine);
     }
 
     // 19
-    public static void quatro(){
+    public static void binaarjatetsimaal(){
         // armas decimal
         int decimal = 56;
-        // printime bsi
+        // printime asju
         System.out.println("Decimal: " + decimal);
         System.out.print("Binary: " + Integer.toBinaryString(decimal));
 
     }
 
     // 21
-    public static void cinco(){
-        int minute = 85;
+    public static void tunnidjaminutid(){
+        int minute = 669;
         // mitu minutit on siis
         int tund = minute / 60;
         int minutes = minute % 60;
-        // paneme selle info stringiks
-        String full = tund + ":" + minutes;
         // printime
-        System.out.println(full);
+        System.out.printf("%d:%02d", tund, minutes);
 
     }
 }
