8bebd51b60004ba2204410325ab3689856022fb5
Marten Laane <113144588+plosee@users.noreply.github.com>
2024-05-21 22:23:42 +0300

    Update kypsised.php

--- tavatood/6-8/kypsised.php
+++ tavatood/6-8/kypsised.php
@@ -3,11 +3,11 @@ session_start();
 $suvalinenummer = rand(1000, 9999);
 $_SESSION['captchatext'] = $suvalinenummer;
 $plit = imagecreatetruecolor(100, 50);
-$tagavarv = imagecolorallocate($im, 255, 255, 255); //must
+$tagavarv = imagecolorallocate($plit, 255, 255, 255); //must
 // valge
-$vaev = imagecolorallocate($im, 0, 0, 0); 
-imagefilledrectangle($im, 0, 0, 100, 50, $tagavarv);
-imagestring($im, 10, 20, 15,  $suvalinenummer, $vaev);
+$vaev = imagecolorallocate($plit, 0, 0, 0); 
+imagefilledrectangle($plit, 0, 0, 100, 50, $tagavarv);
+imagestring($plit, 10, 20, 15,  $suvalinenummer, $vaev);
 header('Content-type: image/png');
 imagepng($plit);
 imagedestroy($plit);
