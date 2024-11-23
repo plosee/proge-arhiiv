0e3ff239086870f62e3f9c7087077c7c1b3d4303
plosee <nopcowe@gmail.com>
2024-11-23 19:06:58 +0200

    javascript algus

---
+++ Javascript/6.js
@@ -0,0 +1,17 @@
+print = console.log
+
+// positiivne voi negatiivne
+number = 2
+switch (number) {
+    case (number > 0):
+        print('number on postiivne')
+        break
+    
+    case (number < 0):
+        print ('number on negatiivne')
+        break
+    
+    case (number == 0):
+        print ('number on null')
+        break
+}
